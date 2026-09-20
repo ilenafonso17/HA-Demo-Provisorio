7ad0f5d55722bc08027eb9a41f66d16f636455fb
ilenafonso17 <152873160+ilenafonso17@users.noreply.github.com>
2026-09-20 01:56:30 +0100

    Distinguir preço manual de referência selecionada

--- app.js
+++ app.js
@@ -287,6 +287,7 @@ function loadReferenceOptions(){
   if(refs.length){ $("p_ref").value="0"; applySelectedReference(); }
   else { $("p_ref").value=""; $("p_brand").value=""; $("p_price").value=""; }
 }
+function markManualPrice(){ if($("p_ref")) $("p_ref").value=""; }
 function applySelectedReference(){
   const name=$("p_prod").value, store=$("p_store").value, refs=sortReferences((db.prices?.references?.[name]||[]).filter(x=>x.store===store));
   const idx=$("p_ref").value;
